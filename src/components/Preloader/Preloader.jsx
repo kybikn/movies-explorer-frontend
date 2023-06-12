859eaa6f7b0ef01d8bb4a5c25c15d449fc8b4d3a
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-12 12:11:58 +0300

    feat: add preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+
 import './Preloader.css'
 
-function Preloader() {
+const Preloader = () => {
   return (
-    <div className='preloader'>
+    <div className="preloader">
       <button className='preloader__button'>Ещё</button>
+      {/* <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div> */}
     </div>
   )
-}
+};
 
 export default Preloader
