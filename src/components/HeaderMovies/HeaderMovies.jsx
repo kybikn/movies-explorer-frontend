2a7226dd6c97f65497cfbc6dda48c0de931f5058
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-12 12:17:46 +0300

    feat: add movies

---
+++ src/components/HeaderMovies/HeaderMovies.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+import NavigationMovies from '../NavigationMovies/NavigationMovies';
+import './HeaderMovies.css';
+import logo from '../../images/logo.svg'
+
+
+function HeaderMovies() {
+  return (
+    <header className="header-movies">
+      <div className="header-movies__container">
+        <Link to='/'><img src={logo} alt="Логотип"></img></Link>
+        <NavigationMovies />
+      </div>
+    </header >
+  )
+}
+
+export default HeaderMovies
---
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import './MoviesCard.css';
+import card from '../../images/card.jpg'
+// import check from '../../images/check.svg'
+
+function MoviesCard() {
+  return (
+    <li>
+      <figure className='card'>
+        <figcaption className='card__description'>
+          <h3 className='card__title'>В погоне за Бенкси</h3>
+          <p className='card__text'>27 минут</p>
+        </figcaption>
+        <img className='card__img' src={card} alt="Карточка фильма"></img>
+        {/* <button
+          className='card__button_active'
+          type='button'
+        ><img src={check} alt="Кнопка"></img></button> */}
+        <button
+          className='card__button'
+          type='button'
+        >Сохранить</button>
+      </figure>
+    </li>
+  )
+}
+
+export default MoviesCard
