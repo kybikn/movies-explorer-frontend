a4e60728f19faa276a5b7e0811a0de0b19018528
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-16 00:52:14 +0300

    feat: add links to Footer and Portfolio

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
+import { footerLinks } from '../../utils/constants'
 import './Footer.css';
 
 function Footer() {
@@ -8,16 +8,18 @@ function Footer() {
     <section className='footer' aria-label="Секция с сылками">
       <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className='footer__content'>
-        <p className='footer__link'>© 2020</p>
+        <p className='footer__link'>&copy; {new Date().getFullYear()}</p>
         <ul className='footer__list'>
-          <li>
-            <Link className="footer__link"
-              to="/">Яндекс.Практикум</Link>
-          </li>
-          <li>
-            <Link className="footer__link"
-              to="/">Github</Link>
-          </li>
+          {footerLinks.map((link, index) =>
+            <li key={index}>
+              <a
+                className="footer__link hover"
+                href={`${link.link}`}
+                target="_blank"
+                rel="noreferrer">{link.name}
+              </a>
+            </li>
+          )}
         </ul>
       </div>
     </section>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
+import { portfolioLinks } from '../../utils/constants'
 import './Portfolio.css';
 import arrow from '../../images/arrow.svg'
-
 
 function Portfolio() {
   return (
     <div>
       <p className='portfolio__title'>Портфолио</p>
       <ul className='portfolio__content'>
-        <li className='portfolio__link'>
-          <h4 className='portfolio__link-title'>Статичный сайт</h4>
-          <Link to="/">
-            <img className="portfolio__link-img" src={arrow} alt="Стрелка"></img>
-          </Link>
-        </li>
-        <li className='portfolio__link'>
-          <h4 className='portfolio__link-title'>Адаптивный сайт</h4>
-          <Link to="/">
-            <img className="portfolio__link-img" src={arrow} alt="Стрелка"></img>
-          </Link>
-        </li>
-        <li className='portfolio__link'>
-          <h4 className='portfolio__link-title'>Одностраничное приложение</h4>
-          <Link to="/">
-            <img className="portfolio__link-img" src={arrow} alt="Стрелка"></img>
-          </Link>
-        </li>
+        {portfolioLinks.map((link, index) =>
+          <li key={index} className='portfolio__link'>
+            <h4 className='portfolio__link-title'>{link.name}</h4>
+            <a
+              href={`${link.link}`}
+              target="_blank"
+              rel="noreferrer">
+              <img
+                className="portfolio__link-img hover"
+                src={arrow}
+                alt={link.name}>
+              </img>
+            </a>
+          </li>
+        )}
+
       </ul>
     </div>
   )
