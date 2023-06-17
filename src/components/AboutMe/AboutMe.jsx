56310f241822c366fea8bf71413611b3180bb8db
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-18 02:46:56 +0300

    fix: add link to Github

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -15,7 +15,11 @@ function AboutMe() {
           <p className='about-me__text_l'>Фронтенд-разработчик, 30 лет</p>
           <p className='about-me__text_m'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
             и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <p className='about-me__text_s'>Github</p>
+          <a
+            className="about-me__text_s hover"
+            href='https://github.com/kybikn'
+            target="_blank" rel="noreferrer">Github
+          </a>
         </div>
         <img className="about-me__img" src={photo} alt="Мое фото"></img>
       </div>
