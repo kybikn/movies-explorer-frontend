2d770abe5b4fa7f0641131f6c8f5e1cd4f9e8dca
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-18 19:58:51 +0300

    fix: change the number of movies in the adaptive SavedMovies

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useBreakpoints } from '../../hooks/useWidth';
 
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SavedDevider from '../SavedDevider/SavedDevider';
-import { savedMovies } from '../../mockData/saved-movies';
+import { savedMovies } from '../../mockData/savedMovies';
 
 function SavedMovies() {
+  const [movies, setMovies] = useState([]);
+
+  let { s, m } = useBreakpoints({
+    s: { min: 0, max: 767 },
+    m: { min: 768, max: null },
+  });
+
+  let cardsAmount;
+  if (s) cardsAmount = 2;
+  if (m) cardsAmount = 3;
+
+  useEffect(() => {
+    const moviesToShow = savedMovies.slice(0, cardsAmount);
+    setMovies(moviesToShow)
+  }, [cardsAmount]);
+
   return (
     <div>
       <SearchForm />
-      <MoviesCardList movies={savedMovies} type="savedMovies" />
+      <MoviesCardList movies={movies} />
       <SavedDevider />
     </div>
   )
