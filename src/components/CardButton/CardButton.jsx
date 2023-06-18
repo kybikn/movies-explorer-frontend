a09e3714cd729f929dfc4f5ce8377b9e51225367
Kybik <82117054+kybikn@users.noreply.github.com>
2023-06-18 13:17:06 +0300

    fix: merge CardButton

--- src/components/CardButton/CardButton.jsx
+++ src/components/CardButton/CardButton.jsx
@@ -1,15 +1,37 @@
-import { React } from 'react';
+import { React, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-import CardButtonDelete from '../CardButtonDelete/CardButtonDelete';
-import CardButtonSave from '../CardButtonSave/CardButtonSave';
 import './CardButton.css';
+import check from '../../images/check.svg'
+import cross from '../../images/cross.svg'
 
-function CardButton({ type }) {
+function CardButton() {
+  const [isButtonActive, setButtonActive] = useState(false);
+
+  const location = useLocation();
+
+  const moviesPage = location.pathname === '/movies';
+
+  function onButtonClick() {
+    setButtonActive(!isButtonActive);
+  }
   return (
     <>
-      {type === 'allMovies'
-        ? <CardButtonSave />
-        : <CardButtonDelete />}
+      {moviesPage
+        ? (<button
+          className={`card__button ${isButtonActive ? "card__button_save" : "card__button_delete"} hover`}
+          onClick={onButtonClick}
+          type='button'
+        >
+          {isButtonActive ? <img src={check} alt="Кнопка"></img> : <p>Сохранить</p>}
+        </button>
+        )
+        : (<button
+          className='card__button card__button_delete hover'
+        >
+          <img src={cross} alt="Кнопка"></img>
+        </button>)
+      }
     </>
   )
 }
